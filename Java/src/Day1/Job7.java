40bb964cbb2e3f5ce1841dc0fabb4471b2403450
mahira-manico <mahira.manico@laplateforme.io>
2026-04-01 15:40:21 +0200

    feat: job7 done

---
+++ Java/src/Day1/Job7.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Job7 {
+    public static void main(String[] args){
+        int sum=0;
+        for(int i=0;i<100;i++){ //we get our 100 integer
+            sum+=1; //we increment sum
+        }
+        System.out.println(sum); //we display the total
+    }
+}
