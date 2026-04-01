691d2115c9e5b6a20d4178e4cf11e301c4a8472a
mahira-manico <mahira.manico@laplateforme.io>
2026-04-01 12:26:00 +0200

    feat: job0 done

---
+++ Java/src/Day1/Job0.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class Job0 {
+    public static void main(String[] args){
+        char gender='F'; //one letter
+        String name="Mahira";
+        int age=21;
+        long year=2026;
+        float gpa=3.56f;
+        boolean isStudent=true;
+        int TOTO=3.817; //Incompatible type, must be double
+        System.out.println(age); //used to display a value
+        System.out.println(name);
+        System.out.println(gender);
+        System.out.println(year);
+        System.out.println(gpa);
+        System.out.println(isStudent);
+
+    }
+}
