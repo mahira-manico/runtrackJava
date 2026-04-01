398da459d9f26c50134a6306f9e61377573b48fc
mahira-manico <mahira.manico@laplateforme.io>
2026-04-01 18:58:59 +0200

    feat: job1 done

--- Java/src/Day2/Toto.java
+++ Java/src/Day2/Toto.java
@@ -1,4 +1,15 @@
 package Day2;
+//error="java: non-static variable toto cannot be referenced from a static context"
 
-public class Toto {
+    class Toto {
+    int toto=0;
+    Toto(){
+        toto=toto+1;
+    }
+    public static void main(String[] tutu){
+        Toto t1=new Toto();
+        Toto t2=new Toto();
+        System.out.println("Toto: ");
+
+    }
 }
