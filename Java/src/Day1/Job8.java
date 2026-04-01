f99ee4f5f4af87838931114ea0015ede6ed25435
mahira-manico <mahira.manico@laplateforme.io>
2026-04-01 15:59:12 +0200

    feat: job8 done

---
+++ Java/src/Day1/Job8.java
@@ -0,0 +1,18 @@
+package Day1;
+import java.util.Scanner;
+
+public class Job8 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int n=sc.nextInt();
+        int sumCube=0;
+        for(int i=1;i<=n;i++){ //a loop for to have all number until n
+            sumCube+=i*i*i; //we use a power calcul
+        }
+        System.out.println("The sum of "+n +" is "+sumCube);
+        sc.close;
+
+
+    }
+}
