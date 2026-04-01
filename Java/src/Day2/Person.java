b880f64005007ede9f6795e4849961687f42f25c
mahira-manico <mahira.manico@laplateforme.io>
2026-04-01 18:48:24 +0200

    feat: add of commentaries to job1 day2

--- Java/src/Day2/Person.java
+++ Java/src/Day2/Person.java
@@ -1,11 +1,11 @@
 package Day2;
 
 public class Person {
-    public String name;
+    public String name; //public can be used and modified by anyone
     public String surname;
-    protected String birthdate;
+    protected String birthdate; //protected can be used and modified only by children or the class herself
     protected String placeOfBrith;
-    private String address;
+    private String address; //with private types and protected types, we need getters and setters to modify the value
     private long phoneNumber;
 
     public Person(String name,String surname,String birthdate, String placeOfBrith,String address,long phoneNumber){
