1ee36aea63ff52967bb3a28147db1d6cf7a4f332
mahira-manico <mahira.manico@laplateforme.io>
2026-04-01 13:39:55 +0200

    feat: job03 done

---
+++ Java/src/Day1/Job03.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class Job03 {
+    public static void main(String[] args){
+        int[] T=new int[10];
+        for(int i=0;i<T.length;i++){
+            T[i]=i;
+        }
+        System.out.println("T[0]="+T[0]);
+        System.out.println("T[1]="+T[1]);
+        System.out.println("T[5]="+T[5]);
+        System.out.println("T[9]="+T[9]);
+        System.out.println("T[10] is out of range");
+    }
+}
