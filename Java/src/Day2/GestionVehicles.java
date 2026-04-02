29d1e7d02b63b9d525d7720bb861080d69e2eab2
mahira-manico <mahira.manico@laplateforme.io>
2026-04-02 13:23:16 +0200

    feat: job 4.1 of day2 done

--- Java/src/Day2/GestionVehicles.java
+++ Java/src/Day2/GestionVehicles.java
@@ -23,3 +23,42 @@ class Vehicle{
     }
 
 }
+
+class Car extends Vehicle{
+    double cylinder;
+    int doorNumbers;
+    int power;
+    long mileage;
+
+    public Car(String brand, String dateOfPurchase, long purchasePrice,double cylinder, int doorNumbers, int power, long mileage ) {
+
+        super(brand, dateOfPurchase, purchasePrice);
+        this.cylinder=cylinder;
+        this.doorNumbers=doorNumbers;
+        this.power=power;
+        this.mileage=mileage;
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("This Car have a cylinder of: "+cylinder+" a door number of: "+doorNumbers+" a power of: "+power+" and a mileage of: "+mileage);
+    }
+}
+
+class AirPlane extends Vehicle{
+    String motorType;
+    int flyHours;
+
+    public AirPlane(String brand, String dateOfPurchase, long purchasePrice, String motorType, int flyHours){
+        super(brand, dateOfPurchase, purchasePrice);
+        this.motorType=motorType;
+        this.flyHours=flyHours;
+    }
+
+    @Override
+    public void display(){
+        super.display();
+        System.out.println("This plane have a motor of type: "+motorType+" and his total of fly hours is: "+flyHours);
+    }
+}
