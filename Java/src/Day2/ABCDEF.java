b0a9fb7496ad3a430420897d11411af809c66250
mahira-manico <mahira.manico@laplateforme.io>
2026-04-02 15:48:30 +0200

    feat: job5 of day2 done

---
+++ Java/src/Day2/ABCDEF.java
@@ -0,0 +1,75 @@
+package Day2;
+
+public class ABCDEF {
+    public static void main(String[] args){
+        A a=new A();
+        B b=new B();
+        C c=new C();
+        D d=new D();
+        E e=new E();
+        F f=new F();
+
+        a=b;
+        // b=d: Incorrect, b cannot be compared to it's parent but a parent can be compared to its child
+        // a=(A)b: Incorrect, b is already a child of A, it is redundant
+        a=null;
+        // null=a: Incorrect because value "a" is always a null by default but null cannot be compared to a value because null si already a value
+        a=d;
+        // b=d: Incorrect because b and d are not related by any inheritance but are cousins
+        a=e;
+        d=e;
+
+        A[] as=new A[10];
+        as[0]=new A();
+        as[1]=new B();
+        as[2]=new D(2);
+        as[3]=new E();
+        as[4]=new C();
+        as[5]=new D(4);
+        as[6]=new B();
+
+        
+
+    }
+}
+
+class A{
+    public A(){}
+}
+
+class B extends A{
+    public B(){
+        super();
+    }
+}
+
+class C extends B{
+    public C(){
+        super();
+    }
+}
+
+class D extends A{
+    protected int d=1;
+
+    public D(int x){
+        super();
+        d=x;
+    }
+
+    public D(){
+
+    }
+}
+
+class E extends D{
+    public E(){
+        super();
+    }
+}
+
+class F extends D{
+    public F(){
+        super();
+    }
+}
